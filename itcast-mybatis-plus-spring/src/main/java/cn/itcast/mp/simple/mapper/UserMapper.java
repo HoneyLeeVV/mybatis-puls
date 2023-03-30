47878807ad464c61b3ed0ad49df0d4c9b97f12db
HoneyLeeVV <941175891@qq.com>
2023-03-30 17:40:24 +0800

    提交spring+mybatis+mp的整合

---
+++ itcast-mybatis-plus-spring/src/main/java/cn/itcast/mp/simple/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package cn.itcast.mp.simple.mapper;
+
+import cn.itcast.mp.simple.pojo.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description
+ * @Author:星海
+ * @CreateTime:2023/3/3017:29
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
---
+++ itcast-mybatis-plus-spring/src/main/java/cn/itcast/mp/simple/pojo/User.java
@@ -0,0 +1,25 @@
+package cn.itcast.mp.simple.pojo;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description
+ * @Author:星海
+ * @CreateTime:2023/3/3017:29
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("tb_user")
+public class User {
+    private Long id;
+    private String userName;
+    private String password;
+    private String name;
+    private Integer age;
+    private String email;
+}
+
